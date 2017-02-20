334f4f393981840a73948c4816a2abd4da65f000
Gustavo Coelho <110030559@aluno.unb.br>
2017-02-20 17:24:31 -0300

    Reading base and servers
    Signed-off-by: Gustavo Coelho <110030559@aluno.unb.br>

--- R/manageFiles.R
+++ R/manageFiles.R
@@ -7,16 +7,34 @@ get_env <- function(){
   files_env
 }
 
+check_variable <- function(var_name){
+  files_env <- get_env()
+  if(exists(var_name, envir = files_env)){
+    return(TRUE)
+  }
+  return(FALSE)
+}
+
+set_variable <- function(var_name, var){
+  files_env <- get_env()
+  files_env[[var_name]] <- var
+}
+
 #' @export
 set_bases_file <- function(bases_file){
   files_env <- get_env()
-  files_env$bases_files <- bases_file
+  files_env$bases_file <- bases_file
 }
 
 get_bases_file <- function(){
   files_env <- get_env()
-  if(exists("bases_files", envir = files_env) & file.exists(files_env$bases_files)){
-    return(files_env$bases_files)
+  if(check_variable("bases_file")){
+    if(file.exists(files_env$bases_file)){
+      return(files_env$bases_file)
+    }else{
+      rm(bases_file, envir = files_env)
+      retrun(get_bases_file())
+    }
   }else if(file.exists(system.file("extdata","bases.csv"))){
     return(system.file("extdata","bases.csv"))
   }else{
@@ -25,6 +43,25 @@ get_bases_file <- function(){
   }
 }
 
-read_bases <- function(){
-
+read_bases <- function(force_read = F){
+  if(check_variable("bases") == F | force_read == T){
+    bases_file <- get_bases_file()
+    bases <- read.csv2(bases_file, skip = 2, stringsAsFactors = F)
+    set_variable("bases", bases)
+  }else{
+    return(get("bases", envir = get_env()))
+  }
+  bases
 }
+
+read_servers <- function(force_read = F){
+  if(check_variable("servers") == F | force_read == T){
+    bases_file <- get_bases_file()
+    servers <- read.csv2(bases_file, nrows = 1, stringsAsFactors = F)
+    set_variable("servers", servers)
+  }else{
+    return(get("servers", envir = get_env()))
+  }
+  servers
+}
+
--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -4,3 +4,47 @@ test_that("Teste get env", {
   files_env <- get_env()
   expect_equal(TRUE, is.environment(files_env))
 })
+
+test_that("Set bases files", {
+  f <- system.file("extdata", "bases.csv", package = "arquivoIPEA")
+  set_bases_file(f)
+  expect_equal(f, get_bases_file())
+})
+
+test_that("Check variables", {
+  result <- check_variable("test")
+  expect_equal(FALSE, result)
+  files_env <- get_env()
+  files_env$test <- 1
+  result <- check_variable("test")
+  expect_equal(TRUE, result)
+})
+
+test_that("Check set variable", {
+  test <- 1
+  set_variable("test", test)
+  result <- check_variable("test")
+  expect_equal(TRUE, result)
+})
+
+test_that("Read servers", {
+  f <- system.file("extdata", "bases.csv", package = "arquivoIPEA")
+  set_bases_file(f)
+  read_from_function <- read_servers()
+  result <- data.frame(server1=c("E:"), server2=c("J:"), X=c(NA), X.1=c(NA), stringsAsFactors = F)
+  expect_equal(read_from_function, result)
+})
+
+test_that("Read bases", {
+  f <- system.file("extdata", "bases.csv", package = "arquivoIPEA")
+  set_bases_file(f)
+  read_from_function <- read_bases()
+  bases <- c("Rais","CadUnico")
+  file.name <- c("rais_2","cad_unico2")
+  dir.input <- c("rais","cad_unico")
+  dir.output <- c("consolidadas/rais", "consolidadas/cad_unico")
+  result <- data.frame(bases=bases, file_name=file.name, dir_input=dir.input, dir_output=dir.output, stringsAsFactors = F)
+  expect_equal(read_from_function, result)
+})
+
+
